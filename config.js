d09494d42d603443821ca77b36c6d17ae3a9fa95
dhaphreddd <127187834+dhaphreddd@users.noreply.github.com>
2023-10-19 19:04:56 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,30 @@
+var config = {
+    
+    texts: [
+        "Buat Finaa ma luvv,",      
+        "Selamat ulang tahun yaa beibb",
+        "Ngueri 20 tahun wes an xixi",
+        "Wish u all the best",
+        "Berdoa o dewe ya, tak aamiin i",
+        "Berdoa mulai....",
+        "Aamiin..",
+        "Tetap semangat dan jaga kesehatan ya kak",
+        "Ambek o aku ae hehe",
+        "Love uuu <3",
+    ],
+
+    imgs: {
+        "Untukmu yang tersayang": "./imgs/fina_copy.png",
+
+    },
+    desc: {
+        turn_on: "Start",
+        play: "Musik",
+        bannar_coming: "Klik",
+        balloons_flying: "Klik maneh",
+        cake_fadein: "Kuenya jgn lupa ygy",
+        light_candle: "Disumet dulu ga si",
+        wish_message: "Happy Birthday luvv<3",
+        story: "for u",
+    }
+};
